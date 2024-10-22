b97b2fe375e49a884fb78756f9726e5a5091ba12
Chindea Eduard <eduard.chindea1@gmail.com>
2024-10-22 19:59:48 +0300

    Added Blossom to update the mod's version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,159 +1,175 @@
+import sct.setVersionFromGit
 import org.apache.commons.lang3.SystemUtils
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     idea
     java
-    id("gg.essential.loom") version "0.10.0.+" // Use the latest Loom version
-    id("dev.architectury.architectury-pack200") version "0.1.3" // Architectury Pack200 support
-    id("com.github.johnrengelman.shadow") version "8.1.1" // Shadow plugin for fat jars
-    kotlin("jvm") version "1.9.0" // Kotlin support
+    id("gg.essential.loom")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("jvm")
+    id("net.kyori.blossom")
 }
 
-// Constants
-val baseGroup: String by project
-val mcVersion: String by project
-val version: String by project
-val modid: String by project
-val transformerFile = file("src/main/resources/accesstransformer.cfg")
+
+version = setVersionFromGit()
 
 // Toolchains
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(8)) // Specify Java version
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
 }
 
-// Minecraft configuration
 loom {
-    log4jConfigs.from(file("log4j2.xml")) // Log4j config
-    launchConfigs {
-        "client" {
-            arg("--tweakClass", "io.github.notenoughupdates.moulconfig.tweaker.DevelopmentResourceTweaker") // Tweak class for NotEnoughUpdates
-        }
-    }
+    log4jConfigs.from(file("log4j2.xml"))
     runConfigs {
         "client" {
             if (SystemUtils.IS_OS_MAC_OSX) {
-                vmArgs.remove("-XstartOnFirstThread") // Prevent crash on macOS
+                vmArgs.remove("-XstartOnFirstThread")
             }
         }
-        remove(getByName("server")) // Remove server run configuration
+        remove(getByName("server"))
     }
     forge {
-        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter()) // Forge pack200 provider
+        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
     }
-}
-
-sourceSets.main {
-    output.setResourcesDir(sourceSets.main.flatMap { it.java.classesDirectory }) // Set resources directory
 }
 
 // Repositories
 repositories {
     mavenCentral()
-    maven("https://repo.spongepowered.org/maven/") // SpongePowered repository
-    maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1") // DevAuth
-    maven("https://repo.nea.moe/releases") // libautoupdate
-    maven("https://maven.notenoughupdates.org/releases/") // NotEnoughUpdates
-    maven("https://jitpack.io"){ // NotEnoughUpdates
+    maven("https://repo.spongepowered.org/maven/")
+    maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
+    maven("https://repo.nea.moe/releases")
+    maven("https://maven.notenoughupdates.org/releases/")
+    maven("https://jitpack.io") {
         content {
             includeGroupByRegex("(com|io)\\.github\\..*")
         }
     }
 }
 
+sourceSets.main {
+    resources.destinationDirectory.set(kotlin.destinationDirectory)
+    output.setResourcesDir(sourceSets.main.flatMap { it.java.classesDirectory })
+}
+
+val kotlinDependencies: Configuration by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
+
 // Shadow configurations
 val shadowImpl: Configuration by configurations.creating {
-    configurations.implementation.get().extendsFrom(this) // Shadow implementation
+    configurations.implementation.get().extendsFrom(this)
 }
 
 val shadowModImpl: Configuration by configurations.creating {
-    configurations.modImplementation.get().extendsFrom(this) // Shadow mod implementation
+    configurations.modImplementation.get().extendsFrom(this)
 }
 
 // Dependencies
 dependencies {
-    minecraft("com.mojang:minecraft:1.8.9") // Minecraft dependency
-    mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9") // MCP mappings
-    forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9") // Forge dependency
+    minecraft("com.mojang:minecraft:1.8.9")
+    mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
+    forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
 
-    implementation(kotlin("stdlib-jdk8")) // Kotlin standard library
+    implementation(kotlin("stdlib-jdk8"))
     shadowImpl("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") {
-        exclude(group = "org.jetbrains.kotlin") // Exclude conflicting Kotlin version
+        exclude(group = "org.jetbrains.kotlin")
     }
 
     shadowModImpl(libs.moulconfig)
     shadowImpl(libs.libautoupdate) {
         exclude(module = "gson")
     }
-    shadowImpl("org.jetbrains.kotlin:kotlin-reflect:1.9.0") // Kotlin reflection
 
-    runtimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.2.1") // Runtime dependency for DevAuth
+    shadowImpl("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
+
+    runtimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.2.1")
+
+    kotlinDependencies(kotlin("stdlib"))
+    kotlinDependencies(kotlin("reflect"))
+
 }
 
 kotlin {
     sourceSets.all {
         languageSettings {
-            languageVersion = "2.0" // Set Kotlin language version
-            enableLanguageFeature("BreakContinueInInlineLambdas") // Enable inline lambdas
+            languageVersion = "2.0"
+            enableLanguageFeature("BreakContinueInInlineLambdas")
         }
     }
 }
 
 // Tasks
 tasks.compileJava {
-    dependsOn(tasks.processResources) // Ensure resources are processed before compilation
+    dependsOn(tasks.processResources)
 }
 
 tasks.withType(JavaCompile::class) {
-    options.encoding = "UTF-8" // Set UTF-8 encoding for Java files
+    options.encoding = "UTF-8"
 }
 
 tasks.withType(org.gradle.jvm.tasks.Jar::class) {
-    archiveBaseName.set(modid) // Set the base name for the jar
+    archiveBaseName.set("SkyblockCollectionTracker")
     manifest.attributes.run {
-        this["FMLCorePluginContainsFMLMod"] = "true" // Mark as a Forge mod
-        this["ForceLoadAsMod"] = "true" // Force load the mod
+        this["FMLCorePluginContainsFMLMod"] = "true"
+        this["ForceLoadAsMod"] = "true"
     }
 }
 
 tasks.processResources {
-    inputs.property("version", project.version)
-    inputs.property("mcversion", mcVersion)
-    inputs.property("modid", modid)
-    inputs.property("basePackage", baseGroup)
-
-    filesMatching(listOf("mcmod.info", "mixins.$modid.json")) {
-        expand(inputs.properties) // Expand properties in resource files
+    inputs.property("version", version)
+    filesMatching(listOf("mcmod.info")) {
+        expand("version" to version)
     }
+}
 
-    rename("accesstransformer.cfg", "META-INF/${modid}_at.cfg") // Rename access transformer file
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_1_8)
+    }
+    dependsOn(tasks.processResources)
 }
 
 // Shadow and relocation configuration
 val remapJar by tasks.named<net.fabricmc.loom.task.RemapJarTask>("remapJar") {
-    archiveClassifier.set("") // Set classifier for remapped jar
-    from(tasks.shadowJar) // From shadow jar
-    input.set(tasks.shadowJar.get().archiveFile) // Input shadow jar
+    archiveClassifier.set("")
+    dependsOn(tasks.shadowJar)
+    inputFile.set(tasks.shadowJar.get().archiveFile)
+    destinationDirectory.set(rootProject.layout.buildDirectory.dir("libs"))
+}
+
+val kotlinDependencyCollectionJar by tasks.creating(Zip::class) {
+    archiveFileName.set("kotlin-libraries-wrapped.jar")
+    destinationDirectory.set(project.layout.buildDirectory.dir("wrapperjars"))
+    from(kotlinDependencies)
+    into("sct-kotlin-libraries-wrapped")
 }
 
 tasks.jar {
-    archiveClassifier.set("without-deps") // Set classifier for jar without dependencies
-    destinationDirectory.set(layout.buildDirectory.dir("intermediates")) // Set destination for jar
+    archiveClassifier.set("without-deps")
+    destinationDirectory.set(layout.buildDirectory.dir("intermediates"))
 }
 
 tasks.shadowJar {
-    destinationDirectory.set(layout.buildDirectory.dir("intermediates")) // Set destination for shadow jar
-    archiveClassifier.set("non-obfuscated-with-deps") // Classifier for shadow jar
+    destinationDirectory.set(layout.buildDirectory.dir("intermediates"))
+    archiveClassifier.set("non-obfuscated-with-deps")
     configurations = listOf(shadowModImpl, shadowImpl)
 
     doLast {
         configurations.forEach {
-            println("Copying dependencies into mod: ${it.files}") // Print copied dependencies
+            println("Copying dependencies into mod: ${it.files}")
         }
     }
-    exclude("META-INF/versions/**") // Exclude specific files
+    exclude("META-INF/versions/**")
 
-    relocate("io.github.notenoughupdates.moulconfig","io.github.chindeaytb.collectiontracker.deps.moulconfig") // Relocate MoulConfig
-    relocate("moe.nea.libautoupdate", "io.github.chindeaytb.collectiontracker.deps.libautoupdate") // Relocate libautoupdate
+    relocate("io.github.notenoughupdates.moulconfig","io.github.chindeaytb.collectiontracker.deps.moulconfig")
+    relocate("moe.nea.libautoupdate", "io.github.chindeaytb.collectiontracker.deps.libautoupdate")
 }
 
-tasks.assemble.get().dependsOn(tasks.remapJar) // Ensure remap jar task runs during assemble
+blossom {
+    replaceToken("@SCT_VERSION@", project.version)
+}
+
+tasks.assemble.get().dependsOn(tasks.remapJar)
