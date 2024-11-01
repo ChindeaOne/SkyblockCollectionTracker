e222514dcb06506f364cf70c46f752e14d257b38
Chindea Eduard <eduard.chindea1@gmail.com>
2024-11-01 21:02:58 +0200

    added a way to check if the player is in Skyblock or not (inspired by Skyhanni)

---
+++ src/main/java/io/github/chindeaytb/collectiontracker/util/Utils.kt
@@ -0,0 +1,17 @@
+/*
+ * This kotlin object is derived from the Skyhanni mod.
+ */
+
+package io.github.chindeaytb.collectiontracker.util
+
+import net.minecraft.client.Minecraft
+
+object Utils {
+
+    val HypixelServer get() = Hypixel.server
+
+    val isInHypixel get() = HypixelServer && Minecraft.getMinecraft().thePlayer != null
+
+    @JvmStatic
+    val isOnSkyblock get() = isInHypixel && Hypixel.skyblock
+}
