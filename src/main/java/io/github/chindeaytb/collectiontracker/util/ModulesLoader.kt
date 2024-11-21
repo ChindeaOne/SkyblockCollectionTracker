9206033433f26a0ca6f0aad79bed9509bba8ee8e
Chindea Eduard <eduard.chindea1@gmail.com>
2024-11-21 12:54:14 +0200

    Now loading all modules from a dedicated class

---
+++ src/main/java/io/github/chindeaytb/collectiontracker/util/ModulesLoader.kt
@@ -0,0 +1,12 @@
+package io.github.chindeaytb.collectiontracker.util
+
+import io.github.chindeaytb.collectiontracker.gui.overlays.CollectionOverlay
+
+object ModulesLoader {
+    val modules: List<Any> = buildList {
+        add(DisconnectHandlerClass())
+        add(CollectionOverlay())
+        add(Hypixel)
+
+    }
+}
