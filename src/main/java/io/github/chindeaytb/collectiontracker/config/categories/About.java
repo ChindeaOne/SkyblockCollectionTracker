c867870bf414679128860bbce05c235474c71870
Chindea Eduard <eduard.chindea1@gmail.com>
2024-11-21 12:47:39 +0200

    Minor change

--- src/main/java/io/github/chindeaytb/collectiontracker/config/categories/About.java
+++ src/main/java/io/github/chindeaytb/collectiontracker/config/categories/About.java
@@ -1,7 +1,7 @@
 package io.github.chindeaytb.collectiontracker.config.categories;
 
 import com.google.gson.annotations.Expose;
-import io.github.chindeaytb.collectiontracker.config.misc.version.VersionDisplay;
+import io.github.chindeaytb.collectiontracker.config.version.VersionDisplay;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorInfoText;
 import io.github.moulberry.moulconfig.annotations.ConfigOption;
 
@@ -14,5 +14,5 @@ public class About {
     @Expose
     @ConfigOption(name = "§aInfo", desc = "This mod is meant to track (almost) any collection that exists. Helps a lot if you are lazy to check your stats.")
     @ConfigEditorInfoText()
-    public boolean info = false;
+    public boolean info = true;
 }
