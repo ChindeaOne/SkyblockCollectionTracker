b884b80ebb9ef7a98f5c7b1b47987f56a14d698f
Chindea Eduard <eduard.chindea1@gmail.com>
2024-11-21 12:51:44 +0200

    Refactoring

--- src/main/java/io/github/chindeaytb/collectiontracker/util/HypixelUtils.kt
+++ src/main/java/io/github/chindeaytb/collectiontracker/util/HypixelUtils.kt
@@ -6,12 +6,13 @@ package io.github.chindeaytb.collectiontracker.util
 
 import net.minecraft.client.Minecraft
 
-object Utils {
+object HypixelUtils {
 
-    val HypixelServer get() = Hypixel.server
+    private val HypixelServer get() = Hypixel.server
 
     val isInHypixel get() = HypixelServer && Minecraft.getMinecraft().thePlayer != null
 
     @JvmStatic
     val isOnSkyblock get() = isInHypixel && Hypixel.skyblock
+
 }
