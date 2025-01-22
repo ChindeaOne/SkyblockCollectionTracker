8ab861eae7ddffdd629d26de4d872b07e4d0e38e
Chindea Eduard <eduard.chindea1@gmail.com>
2025-01-22 22:19:06 +0200

    Removed unused classes

--- src/main/java/io/github/chindeaytb/collectiontracker/util/ModulesLoader.kt
+++ src/main/java/io/github/chindeaytb/collectiontracker/util/ModulesLoader.kt
@@ -4,7 +4,6 @@ import io.github.chindeaytb.collectiontracker.gui.overlays.CollectionOverlay
 
 object ModulesLoader {
     val modules: List<Any> = buildList {
-        add(DisconnectHandlerClass())
         add(CollectionOverlay())
         add(Hypixel)
         add(ServerUtils)
