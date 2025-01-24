11b8724b01403b32857315c2c0bb71f58730922d
Chindea Eduard <eduard.chindea1@gmail.com>
2025-01-24 11:36:23 +0200

    Added the api request for bazaar prices

--- src/main/java/io/github/chindeaytb/collectiontracker/api/URLManager.java
+++ src/main/java/io/github/chindeaytb/collectiontracker/api/URLManager.java
@@ -4,6 +4,7 @@ public class URLManager {
 
     public static String TOKEN_URL = "@TOKEN_URL@";
     public static String COLLECTION_URL = "@COLLECTION_URL@";
+    public static String BAZAAR_URL = "@BAZAAR_URL@";
     public static String STATUS_URL = "@STATUS_URL@";
     public static String AGENT = "@AGENT@";
 }
