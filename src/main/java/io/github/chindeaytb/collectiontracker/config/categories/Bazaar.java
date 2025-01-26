e3726a94bc013b1d38d0eb4ea3f39425fd1ba721
Chindea Eduard <eduard.chindea1@gmail.com>
2025-01-26 22:29:32 +0200

    Changed how bazaar types are handled.

--- src/main/java/io/github/chindeaytb/collectiontracker/config/categories/Bazaar.java
+++ src/main/java/io/github/chindeaytb/collectiontracker/config/categories/Bazaar.java
@@ -1,9 +1,14 @@
 package io.github.chindeaytb.collectiontracker.config.categories;
 
 import com.google.gson.annotations.Expose;
+import io.github.moulberry.moulconfig.annotations.ConfigAccordionId;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorDropdown;
 import io.github.moulberry.moulconfig.annotations.ConfigOption;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Bazaar {
 
@@ -13,15 +18,17 @@ public class Bazaar {
             desc = "Use bazaar prices instead of NPC prices"
     )
     @ConfigEditorBoolean
+    @ConfigAccordionId(id = 0)
     public boolean useBazaar = false;
 
     @Expose
     @ConfigOption(
-            name = "",
+            name = "Select Bazaar Type",
             desc = "Select weather you want to use enchanted or enchanted block version"
     )
     @ConfigEditorDropdown(
-            values = {"Enchanted", "Enchanted Block"}
+            values = {"Enchanted version", "Enchanted block version"}
     )
-    public int bazaarType = 0;
+    @ConfigAccordionId(id = 0)
+    public List<Integer> bazaarType = new ArrayList<>(Arrays.asList(0, 1));
 }
