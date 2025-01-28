7a54b726dfeb9b305d60c926cfb4c0785b39d1c7
Chindea Eduard <eduard.chindea1@gmail.com>
2025-01-28 18:08:36 +0200

    Implemented the new config for colored text

--- src/main/java/io/github/chindeaytb/collectiontracker/config/categories/Overlay.java
+++ src/main/java/io/github/chindeaytb/collectiontracker/config/categories/Overlay.java
@@ -26,7 +26,7 @@ public class Overlay {
     @ConfigEditorDraggableList(
             exampleText = {
                     "§aGold collection §f> 200.000M",
-                    "§aGold collection made §f> 10.000M",
+                    "§aGold collection (session) §f> 10.000M",
                     "§aColl/h §f> Calculating...",
                     "§a$/h (NPC/Bazaar) §f> 100k/h",
             }
@@ -37,4 +37,12 @@ public class Overlay {
     @Expose
     @ConfigLink(owner = Overlay.class, field = "statsOverlay")
     public Position overlayPosition = new Position(4, 150);
+
+    @Expose
+    @ConfigOption(
+            name = "Overlay Text Color",
+            desc = "Toggle this to enable color-coded text."
+    )
+    @ConfigEditorBoolean
+    public boolean overlayTextColor = false;
 }
