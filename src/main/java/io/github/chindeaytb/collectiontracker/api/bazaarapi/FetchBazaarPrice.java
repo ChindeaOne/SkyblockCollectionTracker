40c20b576ce5348d356e111d3afc91daf38d0e44
Chindea Eduard <eduard.chindea1@gmail.com>
2025-08-04 15:52:22 +0300

    Bazaar prices are now set dynamically based on 3 types, as well as the recipes for enchanted and super enchanted version.

--- src/main/java/io/github/chindeaytb/collectiontracker/api/bazaarapi/FetchBazaarPrice.java
+++ src/main/java/io/github/chindeaytb/collectiontracker/api/bazaarapi/FetchBazaarPrice.java
@@ -1,9 +1,10 @@
 package io.github.chindeaytb.collectiontracker.api.bazaarapi;
 
-import io.github.chindeaytb.collectiontracker.ModInitialization;
 import io.github.chindeaytb.collectiontracker.api.URLManager;
-import io.github.chindeaytb.collectiontracker.api.tokenapi.TokenManager;
-import io.github.chindeaytb.collectiontracker.util.PlayerData;
+import io.github.chindeaytb.collectiontracker.collections.BazaarCollectionsManager;
+import io.github.chindeaytb.collectiontracker.collections.CollectionsManager;
+import io.github.chindeaytb.collectiontracker.collections.GemstonesManager;
+import io.github.chindeaytb.collectiontracker.collections.prices.GemstonePrices;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -13,94 +14,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Objects;
 
 public class FetchBazaarPrice {
 
-    public static boolean hasBazaarPrice = true;
-
     private static final Logger logger = LogManager.getLogger(FetchBazaarPrice.class);
 
-    public static String fetchPrice(String uuid, String token, String collection) {
-        try {
-            URL url = new URL(URLManager.BAZAAR_URL);
-            HttpURLConnection conn = getHttpURLConnection(uuid, token, url, collection);
-            int responseCode = conn.getResponseCode();
-            if (responseCode == 200) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                conn.disconnect();
-                return content.toString();
-            } else if (responseCode == 400) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                conn.disconnect();
-                logger.error("[SCT]: Server responded with code 400: {}", content.toString());
-            } else if (responseCode == 401) {
-                logger.warn("[SCT]: Invalid or expired token. Fetching a new one and retrying...");
-                TokenManager.fetchAndStoreToken();
-                conn = getHttpURLConnection(uuid, token, url, collection);
-                responseCode = conn.getResponseCode();
-                if (responseCode == 200) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    String inputLine;
-                    StringBuilder content = new StringBuilder();
-                    while ((inputLine = in.readLine()) != null) {
-                        content.append(inputLine);
-                    }
-                    in.close();
-                    conn.disconnect();
-                    return content.toString();
-                } else {
-                    logger.error("[SCT]: Retry failed. Server responded with code: {}", responseCode);
-                }
-            } else {
-                logger.error("[SCT]: Failed to fetch data. Server responded with code: {}", responseCode);
+    public static void fetchData(String uuid, String token, String collection) {
+        String[] types = {"normal", "enchanted"};
+
+        if(GemstonesManager.checkIfGemstone(collection)) {
+            String type = "gemstone";
+            tryFetchingBazaarData(uuid, token, collection, type);
+        } else {
+            for (String type : types) {
+                tryFetchingBazaarData(uuid, token, collection, type);
             }
-        } catch (Exception e) {
-            logger.error("[SCT]: An error occurred while fetching data from the server: {}", e.getMessage());
         }
-        return null;
     }
 
-    private static @NotNull HttpURLConnection getHttpURLConnection(String uuid, String token, URL url, String collection) throws IOException {
+    private static void tryFetchingBazaarData(String uuid, String token, String collection, String type) {
+        try {
+            Thread.sleep(300);
+            URL url = new URL(URLManager.BAZAAR_URL);
+            HttpURLConnection conn = getHttpURLConnection(uuid, token, url, collection, type);
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder content = new StringBuilder();
+                String line;
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                }
+                in.close();
+                conn.disconnect();
+
+                if(type.equals("gemstone")) {
+                    GemstonePrices.setPrices(content.toString());
+                } else {
+                    BazaarCollectionsManager.setPricesAndRecipes(content.toString(), type);;
+                }
+                // Set collection type for future references
+                CollectionsManager.collectionType = type;
+                logger.info("Bazaar price found for type: {}", type);
+            } else if (responseCode == 404) {
+                logger.warn("Type '{}' not found for collection '{}'", type, collection);
+            } else {
+                logger.warn("Server returned HTTP {} for type '{}'", responseCode, type);
+            }
+        } catch (IOException | InterruptedException e) {
+            logger.error("Error fetching bazaar price for collection '{}': {}", collection, e.getMessage());
+        }
+    }
+
+    private static @NotNull HttpURLConnection getHttpURLConnection(String uuid, String token, URL url, String collection, String type) throws IOException {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("X-UUID", uuid);
         conn.setRequestProperty("Authorization", "Bearer " + token);
         conn.setRequestProperty("X-COLLECTION", collection);
-        conn.setRequestProperty("X-TYPE", String.valueOf(Objects.requireNonNull(ModInitialization.configManager.getConfig()).bazaar.bazaarType));
+        conn.setRequestProperty("X-TYPE", type);
         conn.setRequestProperty("User-Agent", URLManager.AGENT);
         conn.setConnectTimeout(5000); // 5 seconds
-        conn.setReadTimeout(5000); // 5 seconds
+        conn.setReadTimeout(5000);    // 5 seconds
         conn.setRequestProperty("Content-Type", "application/json");
         return conn;
-    }
-
-    public static boolean checkBazaarType(String collection) {
-        try {
-            URL url = new URL(URLManager.CHECK_BAZAAR_TYPE_URL);
-            HttpURLConnection conn = getHttpURLConnection(PlayerData.INSTANCE.getPlayerUUID(), TokenManager.getToken(), url, collection);
-            int responseCode = conn.getResponseCode();
-            if (responseCode == 200) {
-                return true;
-            } else if (responseCode == 400) {
-                hasBazaarPrice = false;
-                return false;
-            }
-        } catch (Exception e) {
-            logger.error("[SCT]: An error occurred while checking the bazaar type: {}", e.getMessage());
-        }
-        return false;
     }
 }
