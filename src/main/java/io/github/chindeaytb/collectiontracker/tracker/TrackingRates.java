d9a5115a21f2fd5070bec3ab5c8ec4d13a3fed26
Chindea Eduard <eduard.chindea1@gmail.com>
2025-08-04 16:05:59 +0300

    Improved profit calculations to keep multiple rates for a collection

--- src/main/java/io/github/chindeaytb/collectiontracker/tracker/TrackingRates.java
+++ src/main/java/io/github/chindeaytb/collectiontracker/tracker/TrackingRates.java
@@ -1,92 +1,108 @@
 package io.github.chindeaytb.collectiontracker.tracker;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import io.github.chindeaytb.collectiontracker.ModInitialization;
+import io.github.chindeaytb.collectiontracker.collections.BazaarCollectionsManager;
 import io.github.chindeaytb.collectiontracker.collections.CollectionsManager;
-import io.github.chindeaytb.collectiontracker.collections.prices.BazaarPrice;
-import io.github.chindeaytb.collectiontracker.collections.prices.NPCPrice;
-import io.github.chindeaytb.collectiontracker.config.categories.Bazaar;
+import io.github.chindeaytb.collectiontracker.collections.prices.BazaarPrices;
+import io.github.chindeaytb.collectiontracker.collections.prices.GemstonePrices;
+import io.github.chindeaytb.collectiontracker.collections.prices.NpcPrices;
+import io.github.chindeaytb.collectiontracker.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.StringReader;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
-import static io.github.chindeaytb.collectiontracker.collections.BazaarCollectionsManager.enchantedCollectionCraft;
+import static io.github.chindeaytb.collectiontracker.collections.CollectionsManager.collectionType;
 import static io.github.chindeaytb.collectiontracker.commands.StartTracker.collection;
 import static io.github.chindeaytb.collectiontracker.tracker.TrackingHandlerClass.getUptimeInSeconds;
 import static io.github.chindeaytb.collectiontracker.util.TextUtils.updateStats;
 
 public class TrackingRates {
 
-    private static final Logger logger = LogManager.getLogger(TrackingRates.class);
+    public static final Logger logger = LogManager.getLogger(TrackingRates.class);
 
     public static float previousCollection = -1;
-    public static float sessionStartCollection = 0;
+    public static float sessionStartCollection = -1;
     public static boolean afk = false;
 
     public static float collectionAmount;
     public static float collectionPerHour;
     public static float collectionMade;
     public static float moneyPerHourNPC;
-    public static float moneyPerHourBazaar;
+    public static Map<String, Float> moneyPerHourBazaar = new HashMap<>();
 
-    public static void displayCollection(String jsonResponse) {
-        try (JsonReader reader = new JsonReader(new StringReader(jsonResponse))) {
-            JsonParser parser = new JsonParser();
-            JsonElement jsonElement = parser.parse(reader);
+    public static void calculateRates(String jsonResponse) {
+        // Set bazaar config
+        ModConfig config = ModInitialization.configManager.getConfig();
+        assert config != null;
 
-            if (jsonElement.isJsonObject()) {
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = parser.parse(jsonResponse).getAsJsonObject();
 
-                if (!jsonObject.entrySet().isEmpty()) {
-                    String key = jsonObject.entrySet().iterator().next().getKey();
-                    float currentCollection = jsonObject.get(key).getAsFloat();
+        float currentCollection = jsonObject.entrySet().iterator().next().getValue().getAsFloat();
 
-                    if (previousCollection > 0) {
-                        if (currentCollection == previousCollection) {
-                            afk = true;
-                            if (TrackingHandlerClass.isTracking) {
-                                TrackingHandlerClass.stopTracking();
-                            }
-                            return;
-                        }
-                    } else {
-                        sessionStartCollection = currentCollection;
-                    }
+        // Set starting collection
+        if (sessionStartCollection == -1) {
+            sessionStartCollection = currentCollection;
 
-                    long uptime = getUptimeInSeconds();
-                    float collectedSinceStart = currentCollection - sessionStartCollection;
-                    float priceNPC, priceBazaar;
-                    Bazaar bazaarConfig = Objects.requireNonNull(ModInitialization.configManager.getConfig()).bazaar;
-
-                    priceBazaar = BazaarPrice.getPrice(collection);
-                    priceNPC = NPCPrice.getNpcPrice(collection);
-
-                    priceNPC = CollectionsManager.notRiftCollection(collection) ? priceNPC : 0;
-                    priceBazaar = CollectionsManager.notRiftCollection(collection) ? priceBazaar : 0;
-
-                    collectionAmount = (float) Math.floor(currentCollection);
-                    collectionPerHour = uptime > 0 ? (float) Math.floor((collectedSinceStart / uptime) * 3600) : 0;
-                    collectionMade = (float) Math.floor(collectedSinceStart);
-                    moneyPerHourNPC = uptime > 0 ? (float) Math.floor(priceNPC * collectedSinceStart / (uptime / 3600.0f)) : 0;
-                    moneyPerHourBazaar = uptime > 0 ? (float) Math.floor(priceBazaar * (collectedSinceStart / enchantedCollectionCraft(collection, bazaarConfig.useBazaar, bazaarConfig.bazaarType)) / (uptime / 3600.0f)) : 0;
-
-                    updateStats();
-
-                    previousCollection = currentCollection;
-                } else {
-                    logger.warn("[SCT]: Collection '{}' not found in the response.", collection);
-                }
-            } else {
-                logger.error("[SCT]: Invalid JSON response: {}", jsonResponse);
-            }
-        } catch (Exception e) {
-            logger.error("[SCT]: An error occurred while processing the collection data", e);
         }
+        // Set player as AFK, else update previousCollection
+        if (currentCollection == previousCollection) {
+            afk = true;
+            if (TrackingHandlerClass.isTracking) {
+                TrackingHandlerClass.stopTracking();
+            }
+            return;
+        } else {
+            previousCollection = currentCollection;
+        }
+
+        long uptime = getUptimeInSeconds();
+        float collectedSinceStart = currentCollection - sessionStartCollection;
+
+        float bazaarPrice;
+        float enchantedPrice;
+        float superEnchantedPrice;
+
+        float priceNPC = NpcPrices.getNpcPrice(collection);
+
+        if(!CollectionsManager.isRiftCollection(collection)) {
+            switch (collectionType) {
+                case "normal":
+                    bazaarPrice = BazaarPrices.normalPrice;
+                    moneyPerHourBazaar.put(collectionType, uptime > 0 ? (float) Math.floor(bazaarPrice * (collectedSinceStart / 160) / (uptime / 3600.0f)) : 0);
+                    break;
+                case "enchanted":
+                    enchantedPrice = BazaarPrices.enchantedPrice;
+                    superEnchantedPrice = BazaarPrices.superEnchantedPrice;
+
+                    moneyPerHourBazaar.put("Enchanted version", uptime > 0 ? (float) Math.floor(enchantedPrice * (collectedSinceStart / BazaarCollectionsManager.enchantedRecipe.values().iterator().next()) / (uptime / 3600.0f)) : 0);
+                    if(superEnchantedPrice == 0.0f){
+                        moneyPerHourBazaar.put("Super Enchanted version", -1.0f);
+                    } else {
+                        moneyPerHourBazaar.put("Super Enchanted version", uptime > 0 ? (float) Math.floor(superEnchantedPrice * (collectedSinceStart / BazaarCollectionsManager.superEnchantedRecipe.values().iterator().next()) / (uptime / 3600.0f)) : 0);
+                    }
+                    break;
+                case "gemstone":
+                    for (String key : GemstonePrices.gemstonePrices.keySet()) {
+                        moneyPerHourBazaar.put(key, uptime > 0 ? (float) Math.floor(GemstonePrices.getPrice(key) * (collectedSinceStart / GemstonePrices.recipes.get(key)) / (uptime / 3600.0f)) : 0);
+                    }
+                    break;
+                default:
+                    logger.warn("[SCT]: Unknown collection type: {}", collectionType);
+            }
+        }
+
+        // Update values
+        collectionAmount = (float) Math.floor(currentCollection);
+        collectionPerHour = uptime > 0 ? (float) Math.floor((collectedSinceStart / uptime) * 3600) : 0;
+        collectionMade = (float) Math.floor(collectedSinceStart);
+        moneyPerHourNPC = uptime > 0 ? (float) Math.floor(priceNPC * collectedSinceStart / (uptime / 3600.0f)) : 0;
+
+        // Update overlay stats
+        updateStats();
     }
 }
-
